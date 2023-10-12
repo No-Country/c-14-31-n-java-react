2297be1640beb6b937c2a4f7523d0f947dd09655
Alejandro <navarro.alejandro@outlook.es>
2023-10-11 19:54:29 -0600

    feat: estructura frontend

---
+++ client/src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/header/Header";
+
+const Home = () => {
+  return (
+    <div className="max-w-screen-xl m-auto">
+      <Header />
+      <h2>Home</h2>
+    </div>
+  );
+};
+
+export default Home;
