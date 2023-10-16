7648eed98de4515c89ca01ea29dda239c5bf4e47
Enzo <91644783+aesaries@users.noreply.github.com>
2023-10-16 13:01:20 -0300

    Creando servicio de Encriptacion de pass

--- api/src/main/java/com/mera/api/service/impl/EncrypServiceImpl.java
+++ api/src/main/java/com/mera/api/service/impl/EncrypServiceImpl.java
@@ -1,2 +1,20 @@
-package com.mera.api.service.impl;public class EncrypServiceImpl {
+package com.mera.api.service.impl;
+
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EncrypServiceImpl implements PasswordEncoder {
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return BCrypt.hashpw((String) rawPassword, BCrypt.gensalt());
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+
+        return BCrypt.checkpw((String) rawPassword, encodedPassword);
+    }
 }
