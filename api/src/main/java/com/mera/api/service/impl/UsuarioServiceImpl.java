5465f470892e47d9fa97e79f14d5e86069d17ba9
Enzo <91644783+aesaries@users.noreply.github.com>
2023-10-16 12:58:31 -0300

    Creando Servicio de Usuario

--- api/src/main/java/com/mera/api/service/impl/UsuarioServiceImpl.java
+++ api/src/main/java/com/mera/api/service/impl/UsuarioServiceImpl.java
@@ -1,19 +1,31 @@
-package com.mera.api.service;
+package com.mera.api.service.impl;
 
 import com.mera.api.dto.request.UsuarioDto;
-import com.mera.api.dto.response.ResUsuarioDto;
+import com.mera.api.dto.request.UsuarioLoginDto;
+import com.mera.api.dto.response.RespStandart;
 import com.mera.api.entity.Usuario;
+
+import com.mera.api.exception.UsuarioFoundException;
+import com.mera.api.exception.UsuarioNotFoundException;
 import com.mera.api.repository.IUsuarioRepository;
+import com.mera.api.service.IUsuarioService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
-public class UsuarioServiceImp implements IUsuarioService {
+public class UsuarioServiceImpl implements IUsuarioService {
 
     @Autowired
     IUsuarioRepository usuarioRepository;
+
+    @Autowired
+    EncrypServiceImpl encrypService;
 
     ModelMapper mapper = new ModelMapper();
     //creamos un ModelMapper(debemos tener la dependencia en el pom).La clase ModelMapper nos permite transformar un objeto relacional en un objeto java
@@ -21,17 +33,88 @@ public class UsuarioServiceImp implements IUsuarioService {
 
 
     @Override
-    public ResUsuarioDto RegistrarUsuario(UsuarioDto usuarioDto) {
+    public RespStandart registrarUsuario(UsuarioDto usuarioDto) {
+        RespStandart resp;
 
+        chequeoExisteMail(usuarioDto.getEmail());
         Usuario usuario = mapper.map(usuarioDto, Usuario.class);
 
         try {
+
+            usuario.setPassword(encrypService.encode(usuario.getPassword()));
             usuarioRepository.save(usuario);
+            resp = new RespStandart("Usuario creado con exito");
 
         }catch (Exception e){
             throw new RuntimeException("Error al Registrar Usuario");
         }
 
+        return resp;
+    }
+
+    @Override
+    public RespStandart loginUsuario(UsuarioLoginDto usuarioLoginDto) {
+        RespStandart resp;
+        Usuario usuario = obtenerDatosUsuario(usuarioLoginDto.getEmail());
+        assert usuario != null;
+
+        Boolean loginOk = verificaPass(usuarioLoginDto.getPassword(), usuario.getPassword());
+        System.out.println(loginOk);
+        if(loginOk){
+             resp = new RespStandart("login Exitoso");
+        }else{
+            resp = new RespStandart("Contraseña Incorrecta");
+        }
+
+
+        return resp;
+    }
+
+
+
+
+    ////////////////////////////////// Metodos de apoyo ////////////////////
+
+    private Boolean verificaPass(String loginPass ,String bbddPass) {
+
+        return encrypService.matches((String)loginPass, bbddPass);
+
+    }
+
+
+
+
+    private void chequeoExisteMail(String email) {
+
+       List<Usuario>listaFiltrada = listaUsuarioFiltrada(email);
+       if (listaFiltrada.size()>0 ){
+           throw new UsuarioFoundException("El mail ya esta en Uso !!");
+       }
+
+    }
+
+
+
+    private Usuario obtenerDatosUsuario(String email) {
+        List<Usuario> lista = listaUsuarioFiltrada(email);
+        if(!(lista.size()==0)){
+            Optional<Usuario> usuarioEncontrado = lista.stream().filter(x -> x.getEmail().equals(email)).findFirst();
+            if(usuarioEncontrado.isPresent()){
+                return usuarioEncontrado.get();
+            }
+        }else{
+            throw new UsuarioNotFoundException("Mail No Encontrado !!");
+        }
+
         return null;
     }
+
+
+    private List<Usuario> listaUsuarioFiltrada(String email){
+        List<Usuario> usuarioList = usuarioRepository.findAll();
+
+        return usuarioList.stream().filter(x-> x.getEmail().equals(email)).toList();
+    }
+
+
 }
