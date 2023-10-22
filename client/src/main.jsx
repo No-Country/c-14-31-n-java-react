0a8f19f7ba1fce739d1c2228d007527c8a2e9f96
Horacio V. T <141278366+horaciodevpro@users.noreply.github.com>
2023-10-22 08:02:51 +0100

    Revert "Horacio"

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
-import { Login } from "./pages/login/Login";
 
 const router = createBrowserRouter([
   {
@@ -14,10 +13,6 @@ const router = createBrowserRouter([
   {
     path: "/register",
     element: <Register />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
   },
 ]);
 
