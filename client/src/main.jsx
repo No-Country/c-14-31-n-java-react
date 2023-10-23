d9fc0d402ed0742affd03822e0948fa5cb701109
LucasIriarte <lucasiriarte67@gmail.com>
2023-10-23 18:41:04 -0300

    feat: add page contact

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import Register from "./pages/register/Register";
 import AboutUs from "./pages/aboutUs/AboutUs";
 import Store from "./pages/store/Store";
 import Faq from "./pages/faq/Faq";
-import Contact from "./pages/contact/Contact"
+import Contact from "./pages/contact/Contact";
 
 const router = createBrowserRouter([
   {
