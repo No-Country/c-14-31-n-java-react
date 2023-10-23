786f331484bc9160d44ed2005b25c713b861231b
Alejandro <navarro.alejandro@outlook.es>
2023-10-22 21:20:42 -0600

    feat/fix: Footer in Home and details

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../../components/header/Header";
 import HomeBody from "../../components/home/Home";
+import { Footer } from "../../components/footer/Footer";
 
 const Home = () => {
   return (
     <div className="max-w-screen-2xl m-auto">
       <Header />
       <HomeBody />
+      <Footer />
     </div>
   );
 };
