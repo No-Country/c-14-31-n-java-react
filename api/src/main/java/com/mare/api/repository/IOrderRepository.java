5d42e4a9e540645fb7acd4884d85431283907b4f
Andres <andres.bozzani@gmail.com>
2023-10-23 23:27:51 -0300

    Archivo de configuracion para conectar con front

--- api/src/main/java/com/mare/api/repository/IOrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mare.api.repository;
-
-import com.mare.api.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IOrderRepository extends JpaRepository<Order, Long> {
-}
