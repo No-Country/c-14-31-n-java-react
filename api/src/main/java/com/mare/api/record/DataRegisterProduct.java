22a344e78dd214e75ecfbd9311acdf869b4fa118
Enzo <91644783+aesaries@users.noreply.github.com>
2023-10-29 20:45:00 -0300

    cambios

--- api/src/main/java/com/mare/api/record/DataRegisterProduct.java
+++ api/src/main/java/com/mare/api/record/DataRegisterProduct.java
@@ -1,7 +1,7 @@
 package com.mare.api.record;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+
 
 public record DataRegisterProduct(Long id,
                                   String name,
