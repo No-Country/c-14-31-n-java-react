c8e137b0c59c342e3e3115f8b2a35ba50f427b98
Alejandro <navarro.alejandro@outlook.es>
2023-10-31 21:45:33 -0600

    feat: updating store component and about

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,9 +1,3 @@
-import Card from "../card/Card";
-import example1 from "../../assets/img/ExampleCard1.png";
-import example2 from "../../assets/img/ExampleCard2.png";
-import example3 from "../../assets/img/ExampleCard3.png";
-import example4 from "../../assets/img/ExampleCard4.png";
-import example5 from "../../assets/img/ExampleCard5.png";
 import carousel from "../../assets/img/mare-indumentaria.jpg";
 import carousel1 from "../../assets/img/carouselImg1.png";
 import carousel2 from "../../assets/img/carouselImg2.png";
@@ -57,11 +51,6 @@ const HomeBody = () => {
           {featured?.map((product) => (
             <CardStore key={product.id} product={product} />
           ))}
-          {/*<Card img={example1} name="Blusa Algodon" price="10100" />
-          <Card img={example2} name="Jeans elastizados" price="18500" />
-          <Card img={example3} name="Suéter lanilla" price="8500" />
-          <Card img={example4} name="Vestidos de fibrana" price="18100" />
-          <Card img={example5} name="Remeras estampadas" price="12200" />*/}
         </div>
       </article>
     </main>
--- client/src/pages/store/CardStore.jsx
+++ client/src/pages/store/CardStore.jsx
@@ -1,28 +1,15 @@
 import { BsCart3, BsHeart } from "react-icons/bs";
-import colors from "../../assets/img/gamaColoresExample.png";
 
 const CardStore = ({ product, handleInfoModal }) => {
   const { name, price, image } = product;
   return (
     <div className="w-[250px] h-[380px] bg-white relative m-1.5 md:h-[341px] md:w-[260px] rounded-tr-[20px] rounded-bl-[20px] border-[0.5px] border-neutral-500 overflow-hidden">
-      {/*{comment && (
-        <div className="w-2/5 absolute top-0 bg-black bg-opacity-50 ">
-          <p className="text-white text-center text-lg  font-semibold tracking-widest font-cormorant">
-            {comment}
-          </p>
-        </div>
-      )}*/}
       <div className="h-24 flex flex-col justify-between absolute top-2 right-0">
-        <div className="w-10 h-10 grid place-content-center rounded-full bg-white bg-opacity-50">
-          <button className="text-2xl ">
-            <BsHeart />
-          </button>
-        </div>
-        <div className="w-10 h-10 grid place-content-center rounded-full bg-white bg-opacity-50">
+        {/*<div className="w-10 h-10 grid place-content-center rounded-full bg-white bg-opacity-50">
           <button className="text-2xl ">
             <BsCart3 />
           </button>
-        </div>
+        </div>*/}
       </div>
       <div className="h-4/5 border-b-[.5px]  border-neutral-500">
         <img className="h-full w-full object-cover" src={image} alt="" />
@@ -35,13 +22,7 @@ const CardStore = ({ product, handleInfoModal }) => {
         <p className="w-24 font-bold leading-7 font-nunito text-left">
           <span>$</span> <span>{price}</span>
         </p>
-        {/*<div>
-          <img
-            src={colors}
-            alt="gama colores"
-            className="ml-auto mt-3 md:w-full"
-          />
-        </div>*/}
+
         <button
           className="w-24 h-9 rounded-md text-white font-nunito bg-primary-400"
           onClick={() => handleInfoModal(product)}>
